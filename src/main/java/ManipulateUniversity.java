65d46f0903405dd9bab80dbce9df36a5750a12c3
MSAC98 <44638120+MSAC98@users.noreply.github.com>
2022-04-05 10:12:56 +0200

    manipulateuniversity

---
+++ src/main/java/ManipulateUniversity.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class ManipulateUniversity {
+    public static void main(String[] args) {
+
+        // Tilpas variable til jeres database.
+        // -----------------------------------
+        String host = "localhost"; //host is "localhost" or "127.0.0.1"
+        String port = "3306"; //port is where to communicate with the RDBM system
+        String database = "university_db"; //database containing tables to be queried
+        String cp = "utf8"; //Database codepage supporting danish (i.e. æøåÆØÅ)
+
+        // Set username og password.
+        // -------------------------
+        String username = "root";		// Username for connection
+        String password = "02327";	// Password for username
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?characterEncoding=" + cp;
+
+        try {
+            Scanner scanner = new Scanner(System.in, "CP850"); //Danish Console CodePage
+            System.out.println("Type sql manipulation: ");
+            String sqlManipulation = scanner.nextLine();
+            scanner.close();
+
+            // Get a connection.
+            // -----------------
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Create and execute Update.
+            // --------------------------
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+
+            // Close connection.
+            // -----------------
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
