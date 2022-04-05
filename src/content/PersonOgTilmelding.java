ad7d0e6540dc1f58d28b3395354fc2c7ce76a6c8
s185736 <65380030+s185736@users.noreply.github.com>
2022-04-05 08:45:21 +0200

    Projektet er oprettet.

---
+++ src/content/PersonOgTilmelding.java
@@ -0,0 +1,34 @@
+package content;
+
+import content.Person;
+
+import java.util.Date;
+
+/**
+ * Denne klasse repræsenterer en content.Person og evt. en tilhørende tilmelding fra en datafil der modtages fra Tidsmaskinens web løsning
+ * 
+ * Klassen er en del af projektopgaven på Kursus 02327 F22
+ * 
+ * @author Thorbjørn Konstantinovitz  
+ *
+ */
+public class PersonOgTilmelding {
+	private final Person person;
+	private final Tilmelding tilmelding;
+
+	public PersonOgTilmelding(String email, String fornavn, String efternavn, String koen, Date foedselsdato, String foreningsId, String eventTypeId, Date eventDato) {
+		person = new Person(email, fornavn, efternavn, koen, foedselsdato);
+		if(foreningsId != null || eventTypeId != null || eventDato != null)
+			tilmelding = new Tilmelding(foreningsId, eventTypeId, eventDato);
+		else
+			tilmelding = null;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public Tilmelding getTilmelding() {
+		return tilmelding;
+	}
+}
